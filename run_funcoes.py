8b6d668f41a4c18376c4fed40591c850233faf47
Rogerio Carvalho Santos <bb.roger@bb.com.br>
2024-05-19 14:43:58 -0300

    ajuste para inclusão da função main

--- run_funcoes.py
+++ run_funcoes.py
@@ -1,13 +1,3 @@
-LIMITE_QUANTIDADE_SAQUES = 3
-LIMITE_VALOR_SAQUE = 500
-AGENCIA = "0001"
-
-clientes = []
-contas = []
-numero_conta = 0
-quantidade_saques = 0
-
-
 def menu():
 
     return """
@@ -30,7 +20,7 @@ def depositar(conta, valor):
         return "Operação falhou! O valor informado é inválido."
 
 
-def sacar(conta, valor):
+def sacar(conta, valor, LIMITE_VALOR_SAQUE, LIMITE_QUANTIDADE_SAQUES):
 
     excedeu_saldo = valor > conta[1]
     excedeu_limite = valor > LIMITE_VALOR_SAQUE
@@ -62,20 +52,19 @@ def visualizar_extrato(conta):
     print("==========================================")
 
 
-def cadastrar_cliente(nome, data_nascimento, cpf, endereco):
+def cadastrar_cliente(nome, data_nascimento, cpf, endereco, clientes):
     cliente = [nome, data_nascimento, cpf, endereco]
     clientes.append(cliente)
     return f"Cliente {nome} cadastrado com sucesso."
 
 
-def cadastrar_conta(cpf):
-    global numero_conta
+def cadastrar_conta(cpf, AGENCIA, numero_conta, quantidade_saques, contas):
     conta = [AGENCIA, 0, [], numero_conta + 1, cpf, quantidade_saques]
     contas.append(conta)
     return f"Conta {conta} cadastrada com sucesso."
 
 
-def verifica_conta(conta):
+def verifica_conta(numero_conta, contas):
     conta_encontrada = None
     for conta in contas:
         if conta[3] == numero_conta:
@@ -84,7 +73,7 @@ def verifica_conta(conta):
     return conta_encontrada
 
 
-def verifica_cpf(cpf):
+def verifica_cpf(cpf, clientes):
     cpf_encontrado = False
     for cliente in clientes:
         if cliente[2] == cpf:
@@ -93,63 +82,82 @@ def verifica_cpf(cpf):
     return cpf_encontrado
 
 
-while True:
-    opcao = input(menu())
+def main():
+    LIMITE_QUANTIDADE_SAQUES = 3
+    LIMITE_VALOR_SAQUE = 500
+    AGENCIA = "0001"
 
-    if opcao == "d":
-        numero_conta = int(input("Informe o número da conta: "))
-        conta = verifica_conta(numero_conta)
-        if not conta:
-            print("Conta não localizada.")
-        else:
-            valor = float(input("Informe o valor do depósito: "))
-            print(depositar(conta, valor))
+    clientes = []
+    contas = []
+    numero_conta = 0
+    quantidade_saques = 0
 
-    elif opcao == "s":
-        numero_conta = int(input("Informe o número da conta: "))
-        conta = verifica_conta(numero_conta)
-        if not conta:
-            print("Conta não localizada.")
-        else:
-            valor = float(input("Informe o valor do saque: "))
-            print(sacar(conta, valor))
+    while True:
+        opcao = input(menu())
 
-    elif opcao == "e":
-        numero_conta = int(input("Informe o número da conta: "))
-        conta = verifica_conta(numero_conta)
-        if not conta:
-            print("Conta não localizada.")
-        else:
-            visualizar_extrato(conta)
+        if opcao == "d":
+            numero_conta = int(input("Informe o número da conta: "))
+            conta = verifica_conta(numero_conta, contas)
+            if not conta:
+                print("Conta não localizada.")
+            else:
+                valor = float(input("Informe o valor do depósito: "))
+                print(depositar(conta, valor))
 
-    elif opcao == "u":
-        cpf = int(input("Informe o CPF do cliente (somente números): "))
-        if verifica_cpf(cpf):
-            print("Cliente já cadastrado.")
+        elif opcao == "s":
+            numero_conta = int(input("Informe o número da conta: "))
+            conta = verifica_conta(numero_conta, contas)
+            if not conta:
+                print("Conta não localizada.")
+            else:
+                valor = float(input("Informe o valor do saque: "))
+                print(sacar(conta, valor, LIMITE_VALOR_SAQUE, LIMITE_QUANTIDADE_SAQUES))
+
+        elif opcao == "e":
+            numero_conta = int(input("Informe o número da conta: "))
+            conta = verifica_conta(numero_conta, contas)
+            if not conta:
+                print("Conta não localizada.")
+            else:
+                visualizar_extrato(conta)
+
+        elif opcao == "u":
+            cpf = int(input("Informe o CPF do cliente (somente números): "))
+            if verifica_cpf(cpf, clientes):
+                print("Cliente já cadastrado.")
+            else:
+                nome = str(input("Informe o nome do cliente: "))
+                data_nascimento = str(
+                    input("Informe a data nascimento (dd-mm-aaaa) do cliente: ")
+                )
+
+                logradouro = str(input("Informe o logradouro do cliente: "))
+                numero = int(input("Informe o número do logradouro do cliente: "))
+                bairro = str(input("Informe o bairro do cliente: "))
+                cidade = str(input("Informe a cidade do cliente: "))
+                uf = str(input("Informe a uf do cliente: "))
+                endereco = f"{logradouro}, {numero} - {bairro} - {cidade}/{uf}"
+
+                print(cadastrar_cliente(nome, data_nascimento, cpf, endereco, clientes))
+
+        elif opcao == "c":
+            cpf = int(input("Informe o CPF do cliente (somente números): "))
+            if not verifica_cpf(cpf, clientes):
+                print("Cliente não localizado.")
+            else:
+                print(
+                    cadastrar_conta(
+                        cpf, AGENCIA, numero_conta, quantidade_saques, contas
+                    )
+                )
+
+        elif opcao == "q":
+            break
+
         else:
-            nome = str(input("Informe o nome do cliente: "))
-            data_nascimento = str(
-                input("Informe a data nascimento (dd-mm-aaaa) do cliente: ")
+            print(
+                "Operação inválida, por favor selecione novamente a operação desejada."
             )
 
-            logradouro = str(input("Informe o logradouro do cliente: "))
-            numero = int(input("Informe o número do logradouro do cliente: "))
-            bairro = str(input("Informe o bairro do cliente: "))
-            cidade = str(input("Informe a cidade do cliente: "))
-            uf = str(input("Informe a uf do cliente: "))
-            endereco = f"{logradouro}, {numero} - {bairro} - {cidade}/{uf}"
 
-            print(cadastrar_cliente(nome, data_nascimento, cpf, endereco))
-
-    elif opcao == "c":
-        cpf = int(input("Informe o CPF do cliente (somente números): "))
-        if not verifica_cpf(cpf):
-            print("Cliente não localizado.")
-        else:
-            print(cadastrar_conta(cpf))
-
-    elif opcao == "q":
-        break
-
-    else:
-        print("Operação inválida, por favor selecione novamente a operação desejada.")
+main()
